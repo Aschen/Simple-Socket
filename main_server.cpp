b147471d2b58e394bc9bd91178a5c65fa7e824e2
Adrien Maret <maret_a@epitech.eu>
2014-05-27 11:19:33 +0200

    modif exemple server

--- main_server.cpp
+++ main_server.cpp
@@ -12,8 +12,8 @@ int	main(void)
   server.addMessage(Message("TYPE", "MESSAGE"));
   server.addMessage(Message("TEST", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru."));
 
-  while (server.getWriteQueueSize())
-    server.writeMsg(server_handle.get());
+  while (server.writeMsg(server_handle.get()))
+    ;
 
   server.stop();
   return 0;
